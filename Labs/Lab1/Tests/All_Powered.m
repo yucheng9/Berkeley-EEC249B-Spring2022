908b6591c01070b38da970c802601a6cbc67f065
Yucheng Jin <yucheng9@illinois.edu>
2022-04-06 18:03:53 -0700

    final

---
+++ Labs/Lab1/Tests/All_Powered.m
@@ -0,0 +1,9 @@
+% 999 means the generator will not be unhealthy
+
+set_param('Lab1AircraftEPS/Sig','Time','999');
+
+set_param('Lab1AircraftEPS/Sig1','Time','999');
+
+set_param('Lab1AircraftEPS/Sig2','Time','999');
+
+set_param('Lab1AircraftEPS/Sig3','Time','999');
---
+++ Labs/Lab1/Tests/GrAr_Down.m
@@ -0,0 +1,10 @@
+% Assign time when the step signal changes
+% 999 means the generator will not be unhealthy
+
+set_param('Lab1AircraftEPS/Sig','Time','999');
+
+set_param('Lab1AircraftEPS/Sig1','Time','999');
+
+set_param('Lab1AircraftEPS/Sig2','Time','2');
+
+set_param('Lab1AircraftEPS/Sig3','Time','1');
